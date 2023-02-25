59445555d7dec3a54f712ff07ba7b8f95a51d325
Rishit Shah <aryanrishit1@gmail.com>
2023-02-25 15:10:34 +0530

    First Python Commit

---
+++ variables.py
@@ -0,0 +1,21 @@
+# this is comment! Python doesn't have multiline comments :(
+
+#first_name = "Rishit" # strings can use '' and ""
+#last_name = "Shah"
+#full_name = first_name + " " + last_name
+
+#print(type(full_name)) #prints data type of variable
+#print("Hello " + full_name)
+
+#age = 19
+#age += 1 # no increment or decrement operators in Python :(
+#print("Your age is: " + str(age)) # str stands for string, here age is being type cast into string to be able to print it with the other text
+#print(type(age))
+
+#height = 250.5
+#print("Your height is: " + str(height) + " cm") # same thing as the int
+#print(type(height))
+
+human = True # True and False have to have the first letter capital in Python
+print("You are a human: " + str(human))
+#print(type(human))
