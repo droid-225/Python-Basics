9b269f8fa60ff9f7c45217fde00d49077634b258
Rishit Shah <aryanrishit1@gmail.com>
2023-02-25 15:29:51 +0530

    Learned String Methods

---
+++ String_Methods.py
@@ -0,0 +1,12 @@
+name = "bro"
+
+#print(len(name)) # prints length of string
+#print(name.find("r")) # prints index of character in string
+#print(name.capitalize()) # capitalizes first letter of first word in string
+#print(name.upper()) # prints string in all uppercase
+#print(name.lower()) # prints string in all lowercase
+#print(name.isdigit()) # prints if string is a digit
+#print(name.isalpha()) # prints if all characters in string is alphabetical letters (special characters like spaces in the middle will cause it to say false even if all other characters are letters)
+#print(name.count("o")) # returns amount of times a character is found in string
+#print(name.replace("o", "a")) # .replace(old character, new character)
+print(name * 3) #prints string specified number of times
